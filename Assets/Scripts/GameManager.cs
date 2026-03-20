451e39b26b08b1aaa9a035177e6ab8610c551785
unknown <toririowl@gmail.com>
2026-03-20 14:14:44 -0500

    Compressed Game Manager and button scripts into GameManager and fixed button/gamemanager related bugs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,35 @@
-using System.Diagnostics;
-using System.Runtime.Serialization;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.InputSystem;
 using UnityEngine.AI;
-
-public class GameManager : MonoBehaviour
+using System.Runtime.Serialization;
+public class ButtonManager : MonoBehaviour
 {
-    
-    
+    // Keeps track of if the game is paused.
+    public static bool isPaused = false;
+    // Keeps track of the pause button.
+    private InputAction pauseButton;
+    // Refers to the Pause Screen in the Hierarchy.
+    [SerializeField] private GameObject PauseScreen;
     [SerializeField] private int picnicBasketHealth;
-    [SerializeField] private bool isGameOver;
+    public static bool isGameOver;
     [SerializeField] private GameObject GameOverScreen;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    public void Start()
     {
-        
+        // Grabs the input from the pause button. (good)
+        pauseButton = InputSystem.actions.FindAction("Pause");
     }
-
     // Update is called once per frame
     void Update()
     {
+        //If they press pause button, toggle Pause. (maybe "pause the game")
+        if (pauseButton.WasPressedThisFrame() && isGameOver == false)
+        {
+            TogglePause();
+        }
+        
         if (picnicBasketHealth <= 0)
         {
             if (!isGameOver)
@@ -28,10 +38,87 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+
+    public void SceneChange()
+    {
+            // Checks if the game is paused or not and if it is, it resets the timeScale and resets the isPaused boolean.
+            
+            
+            Time.timeScale = 1.0f;
+            isPaused = false;
+            isGameOver = false;
+                    
+            
+
+            // If the current scene is not Main Menu, load the main menu.
+            if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("MainMenu"))
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+            // If the current scene is the main menu load the gamescene.
+            else
+            {
+                SceneManager.LoadScene("SampleScene_UI");
+            }
+        
+    }
+    public void RestartGame() 
+    {
+            // Loads the current scene the button was pressed on.
+    		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1.0f;
+            isPaused = false;
+            isGameOver = false;
+            picnicBasketHealth = 100;
+    }
+
+    public void ExitGame()
+    {
+        // Closes the application.
+        Application.Quit();
+        //Test case if you are in the Unity Editor.
+        Debug.Log("Game is exiting");
+        
+        //exits the Play state if you are in the unity editor as opposed to a game itself.
+        if (Application.isEditor)
+        {
+             UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+    // Handles Pausing the game.
+    public void TogglePause()
+    {
+        // Toggles the pause state. If it was unpaused, it is now paused.
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Debug.Log("Game is paused");
+            if(!PauseScreen.activeInHierarchy)
+            {
+            // Makes the Pause Screen appear on screen.
+            PauseScreen.SetActive(true);
+            }
+            // Halts the everpresent, oppressive, looming presence of time 
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Debug.Log("Game is unpaused");
+            // Removes Pause Screen from the screen.
+            if(PauseScreen.activeInHierarchy)
+            {
+                PauseScreen.SetActive(false);
+            }
+            // Continues our lifelong march toward death.
+            Time.timeScale = 1.0f;
+        }
+    }
     public void CalculatePicnicHP(int antDamage)
     {
         picnicBasketHealth -= antDamage;
-        UnityEngine.Debug.Log("Picnic Basket Health Is Now: " + picnicBasketHealth);
+        Debug.Log("Picnic Basket Health Is Now: " + picnicBasketHealth);
     }
     public void GameOver()
     {
@@ -39,17 +126,22 @@ public class GameManager : MonoBehaviour
         {
             if (isGameOver)
             {
-                UnityEngine.Debug.Log("Game Over!");
+                Debug.Log("Game Over!");
                 Time.timeScale = 0.0f;
-                GameOverScreen.SetActive(true);
-                return;
+                if (!GameOverScreen.activeInHierarchy)
+                {
+                    GameOverScreen.SetActive(true);
+                }
             }
             else
             {
                 Time.timeScale = 1.0f;
-                GameOverScreen.SetActive(false);
-                return;
+                if (GameOverScreen.activeInHierarchy)
+                {
+                    GameOverScreen.SetActive(false);
+                }
             }
         }
     }
+    
 }
