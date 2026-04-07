924194209729dfe69b57b2ef5dd2b4cebd51f9f2
unknown <frazier2020m@gmail.com>
2026-04-07 14:26:33 -0500

    WIP: saving changes before switching branches

---
+++ Assets/Scripts/Tower Scripts/CubeMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    // Determines the speed of the cube.
+    [SerializeField] float speed = 5.0f;
+
+    void Update()
+    {
+        // Moves the object to the right over time
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
