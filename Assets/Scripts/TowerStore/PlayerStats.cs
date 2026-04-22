0f83203ffff59a7c6b96b5f5c3586bce1880c7da
unknown <tpewitt080507@gmail.com>
2026-04-22 14:46:00 -0500

    Money Fix

--- Assets/Scripts/TowerStore/PlayerStats.cs
+++ Assets/Scripts/TowerStore/PlayerStats.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public static int Money;
-    public int startMoney = 10;
+    private int startMoney = 10;
 
     void Start()
     {
